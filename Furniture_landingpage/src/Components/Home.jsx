86ae391b026fa5feff561af730ed2fdf9d01fc2a
Vishal Ingle <vishaling208@gmail.com>
2025-06-28 16:39:53 +0530

    Initial commit

---
+++ Furniture_landingpage/src/Components/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div className="text-3xl font-bold underline" style={{
+        backgroundImage: "url('https://images.unsplash.com/photo-1600585154340-2f8b1c3d4e5a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80')",
+    }}>
+      Furniture
+    </div>
+  )
+}
+
+export default Home
